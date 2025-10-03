03cad371a5690bc310a6bb5460475d43813bb997
coolstar.arina@gmail.com <eparis@atlassian.com>
2025-10-03 16:59:27 +0300

    2099

--- eolymp.py
+++ eolymp.py
@@ -3,33 +3,49 @@
 
 # 5058
 
-n = input()
-opening_layers = {"(": [], "{": [], "[": []}
-brackets = {")":"(", "}":"{", "]":"["}
-i = 0 # layers
-res = "yes"
-for br in n:
-    if br in opening_layers.keys():
-        opening_layers[br].append(i)
-        i += 1
+# n = input()
+# opening_layers = {"(": [], "{": [], "[": []}
+# brackets = {")":"(", "}":"{", "]":"["}
+# i = 0 # layers
+# res = "yes"
+# for br in n:
+#     if br in opening_layers.keys():
+#         opening_layers[br].append(i)
+#         i += 1
+#     else:
+#         l = opening_layers[brackets[br]]
+#         if len(l) != 0:
+#             if i-1 == l[len(l)-1]:
+#                 i -= 1
+#                 l = l[:len(l)-1]
+#                 opening_layers[brackets[br]] = l
+#             else:
+#                 res = "no"
+#                 break
+#         else:
+#             res = "no"
+#             break
+# for l_br in opening_layers.values():
+#     if len(l_br) != 0:
+#         res = "no"
+# print(res)
+
+
+# 2099
+
+
+n = int(input())
+nl = input().split()
+m = int(input())
+ml = input().split()
+res = []
+
+for l in nl:
+    if l not in ml:
+        res.append(l)
+print(len(res))
+for r in range(len(res)):
+    if r != len(res)-1:
+        print(res[r], end=" ")
     else:
-        l = opening_layers[brackets[br]]
-        if len(l) != 0:
-            if i-1 == l[len(l)-1]:
-                i -= 1
-                l = l[:len(l)-1]
-                opening_layers[brackets[br]] = l
-            else:
-                res = "no"
-                break
-        else:
-            res = "no"
-            break
-for l_br in opening_layers.values():
-    if len(l_br) != 0:
-        res = "no"
-print(res)
-
-
-
-
+        print(res[r])
